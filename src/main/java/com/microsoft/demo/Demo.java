de82d9e6e6c7edb94cdfeeed792d28a1cf521c5a
duochen <duochen@yahoo.com>
2019-06-13 16:22:12 -0500

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,9 +3,9 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered");
+            System.out.println("I am covered!");
             return;
         }
-        System.out.println("I am not covered");
+        System.out.println("I am not covered!");
     }
 }
